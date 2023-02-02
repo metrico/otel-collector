024aebd204078f1dafbb6941db48b4893cc939e2
Cluas <Cluas@live.cn>
2023-02-02 19:15:00 +0800

    chore: add comment

--- exporter/qrynexporter/config.go
+++ exporter/qrynexporter/config.go
@@ -36,7 +36,9 @@ type Config struct {
 	// For http protocol reference: [mailru/go-clickhouse/#dsn](https://github.com/mailru/go-clickhouse/#dsn).
 	DSN string `mapstructure:"dsn"`
 
-	Logs    LogsConfig    `mapstructure:"logs"`
+	// Logs is used to configure the log data.
+	Logs LogsConfig `mapstructure:"logs"`
+	// Metrics is used to configure the metric data.
 	Metrics MetricsConfig `mapstructure:"metrics"`
 }
 
@@ -46,14 +48,19 @@ type QueueSettings struct {
 	QueueSize int `mapstructure:"queue_size"`
 }
 
+// LogsConfig holds the configuration for log data.
 type LogsConfig struct {
+	// AttritubeLabels is the string representing attribute labels.
 	AttritubeLabels string `mapstructure:"attritube_labels"`
-	ResourceLabels  string `mapstructure:"resource_labels"`
-	Format          string `mapstructure:"format"`
+	// ResourceLabels is the string representing resource labels.
+	ResourceLabels string `mapstructure:"resource_labels"`
+	// Format is the string representing the format.
+	Format string `mapstructure:"format"`
 }
 
+// MetricsConfig holds the configuration for metric data.
 type MetricsConfig struct {
-	// prefix attached to each exported metric name
+	// Namespace is the prefix attached to each exported metric name.
 	// See: https://prometheus.io/docs/practices/naming/#metric-names
 	Namespace string `mapstructure:"namespace"`
 }
