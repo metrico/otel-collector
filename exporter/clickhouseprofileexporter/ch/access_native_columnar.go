41b785a1ae31a3476e6eaf510a47ad34caa1ce60
tomershafir <tomer.shafir8@gmail.com>
2024-02-07 13:23:15 +0200

    fix payload_type

--- exporter/clickhouseprofileexporter/ch/access_native_columnar.go
+++ exporter/clickhouseprofileexporter/ch/access_native_columnar.go
@@ -159,7 +159,7 @@ func (ch *clickhouseAccessNativeColumnar) InsertBatch(ls plog.Logs) error {
 		tmp, _ = m.Get(columnDurationNs)
 		duration_ns[i], _ = strconv.ParseUint(tmp.Str(), 10, 64)
 
-		tmp, _ = m.Get(columnPeriodType)
+		tmp, _ = m.Get(columnPayloadType)
 		payload_type[i] = tmp.AsString()
 
 		payload[i] = r.Body().Bytes().AsRaw()
